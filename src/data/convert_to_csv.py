0ca34d842f3765144cef009f9b7b410ec4125418
Nimrod Milo <nimrodm@traxretail.com>
2017-08-31 22:36:00 +0300

    Convert script now writes everything to bzip file. Also, it looks for sub_folders

--- src/data/convert_to_csv.py
+++ src/data/convert_to_csv.py
@@ -2,14 +2,15 @@ import pandas as pd
 import os
 import glob
 from tqdm import tqdm
+import bz2
 
 DATA_DIR = "data"
 
-output_file = os.path.join(DATA_DIR, "data.csv")
+output_file = os.path.join(DATA_DIR, "data.csv.bz2")
 if os.path.isfile(output_file):
     os.unlink(output_file)
-with open(output_file, 'a') as f:
-    files = glob.glob(os.path.join(DATA_DIR, "*.pkl"))
+with bz2.BZ2File(output_file, 'wb') as f:
+    files = glob.glob(os.path.join(DATA_DIR, "**/*.pkl"))
     df = pd.read_pickle(files[0])
     df.to_csv(f, header=True, encoding='windows-1255')
     del df
